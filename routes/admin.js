16fe14d27c98a777048c1836207fe2378049e781
Alina <alina.pamfile@yahoo.com>
2020-05-01 21:24:39 +0300

    getFormsByEmail

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,8 @@
 const router = require('express').Router()
 const { adminController } = require('../controllers')
 
-router.get('/all_forms', adminController.getAllForms)
+router.get('/forms', adminController.getForms)
 router.get('/applicants', adminController.getApplicants)
+router.get('/forms/:email', adminController.getFormsByEmail)
 
 module.exports = router
